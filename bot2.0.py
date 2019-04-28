99dc6490d62f3b21f9f97eaf76a916a57866d003
MadNiklek <madniklek@gmail.com>
2019-04-28 22:09:53 +0300

    a

--- bot2.0.py
+++ bot2.0.py
@@ -1,96 +1,50 @@
 import logging
+import telegram
+from telegram.error import NetworkError, Unauthorized
+from time import sleep
+import requests
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Kiev','us&appid=6615c7ed8550b538ed7b2d68919039e9')
+json_object = r.json()
+mainn = json_object["weather"][0]["main"]
+descriptionn = json_object["weather"][0]["description"]
 
-from telegram.ext import Updater, CommandHandler,  MessageHandler, Filters
+update_id = None
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-def start(update, context):
-    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
-
-
-def alarm(context):
-    """Send the alarm message."""
-    job = context.job
-    context.bot.send_message(job.context, text='Beep!')
-
-
-def set_timer(update, context):
-    chat_id = update.message.chat_id
-    try:
-        # args[0] should contain the time for the timer in seconds
-        due = int(context.args[0])
-        if due < 0:
-            update.message.reply_text('Sorry we can not go back to future!')
-            return
-
-        # Add job to queue
-        job = context.job_queue.run_once(alarm, due, context=chat_id)
-        context.chat_data['job'] = job
-
-        update.message.reply_text('Timer successfully set!')
-
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /set <seconds>')
-
-
-def unset(update, context):
-    """Remove the job if the user changed their mind."""
-    if 'job' not in context.chat_data:
-        update.message.reply_text('You have no active timer')
-        return
-
-    job = context.chat_data['job']
-    job.schedule_removal()
-    del context.chat_data['job']
-
-    update.message.reply_text('Timer successfully unset!')
-
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def echo(update, context):
-	if update.message.text == "stop":
-		updater.stop()
-	elif update.message.text == "continue":
-		updater.start_polling()
-
-updater = Updater("873804634:AAFdPWVQSSc00kndarzPiFryLgHUqt7E6aM", use_context=True)
 
 def main():
-    global updater
+    """Run the bot."""
+    global update_id
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot('873804634:AAFdPWVQSSc00kndarzPiFryLgHUqt7E6aM')
 
-    dp = updater.dispatcher
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
+    try:
+        update_id = bot.get_updates()[0].update_id
+    except IndexError:
+        update_id = None
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", start))
-    dp.add_handler(CommandHandler("set", set_timer,
-                                  pass_args=True,
-                                  pass_job_queue=True,
-                                  pass_chat_data=True))
-    dp.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # log all errors
-    dp.add_error_handler(error)
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    updater.start_polling()
+    while True:
+        try:
+            echo(bot)
+        except NetworkError:
+            sleep(1)
+        except Unauthorized:
+            # The user has removed or blocked the bot.
+            update_id += 1
 
-    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
-    # SIGABRT. This should be used most of the time, since start_polling() is
-    # non-blocking and will stop the bot gracefully.
-    updater.idle()
+
+def echo(bot):
+    """Echo the message the user sent."""
+    global update_id
+    # Request updates after the last update_id
+    for update in bot.get_updates(offset=update_id, timeout=10):
+        update_id = update.update_id + 1
+        if update.message.text == "/weather":
+            update.message.reply_text("Now is " + mainn + ", to be more precise it's " + descriptionn)
 
 
 if __name__ == '__main__':
     main()
-# cd desktop\new_bot
-# git add .
-# git commit -m "m"
-# git push origin
-
-# 873804634:AAFdPWVQSSc00kndarzPiFryLgHUqt7E6aM	
